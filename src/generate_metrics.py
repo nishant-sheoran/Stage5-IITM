7a31424da62b34685050c78d9b9bef8d79a2b133
Nishant Sheoran <ns3255@srmist.edu.in>
2025-05-09 17:51:13 +0000

    added Total Number of Instructions in generate_metrics

--- src/generate_metrics.py
+++ src/generate_metrics.py
@@ -6,6 +6,7 @@ def generate_metrics(perm, head_cont, cycles, tot_ins, io_dir: Path):
 
     content = [head_cont + "\n",
                f"Number of cycles taken: {cycles}\n",
+               f"Total Number of Instructions: {tot_ins}\n",
                f"Cycles per instruction: {(cycles / tot_ins):.6}\n",
                f"Instructions per cycle: {(tot_ins / cycles):.6}\n\n"]
 
